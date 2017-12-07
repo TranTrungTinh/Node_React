a02c569a6a5998d95662b4eca2bf710b29a3e1eb
Trung Tinh <trantrungtinh4954@gmail.com>
2017-12-07 21:36:22 +0700

    add redux-thunk

--- client/src/redux/actionCreators.js
+++ client/src/redux/actionCreators.js
@@ -1,30 +1,85 @@
 export function login(id , email , firstname , lastname , amount){
-  return { type: 'LOG_IN' , id , email, firstname , lastname , amount };
+  // return { type: 'LOG_IN' , id , email, firstname , lastname , amount };
+  return dispatch => {
+    dispatch({
+      type: 'LOG_IN',
+      id , email, firstname , lastname , amount
+    });
+  };
 } 
 export function logout() {
-  return { type: 'LOG_OUT' };
+  return dispatch => {
+    dispatch({
+      type: 'LOG_OUT'
+    });
+  };
+  // return { type: 'LOG_OUT' };
 }
 export function islogin() {
-  return { type: 'IS_LOG_IN'};
+  return dispatch => {
+    dispatch({
+      type: 'IS_LOG_IN'
+    });
+  };
+  // return { type: 'IS_LOG_IN'};
 }
 export function updateAmount(amount) {
-  return { type: 'UPDATE_AMOUNT' , amount};
+  return dispatch => {
+    dispatch({
+      type: 'UPDATE_AMOUNT', 
+      amount
+    });
+  };
+  // return { type: 'UPDATE_AMOUNT' , amount};
 }
 export function addTranfers(nameSend, nameReceive, money) {
-  return { type: 'ADD_TRANFERS' , nameSend, nameReceive, money};
+  return dispatch => {
+    dispatch({
+      type: 'ADD_TRANFERS',
+      nameSend, nameReceive, money
+    });
+  };
+  // return { type: 'ADD_TRANFERS' , nameSend, nameReceive, money};
 }
 export function clearTranfers(){
-  return {type: 'CLEAR_TRANFERS'};
+  return dispatch => {
+    dispatch({
+      type: 'CLEAR_TRANFERS'
+    });
+  };
+  // return {type: 'CLEAR_TRANFERS'};
 }
 export function addSendHistory(nameSend, nameReceive, money) {
-  return { type: 'SEND_HISTORY' , nameSend, nameReceive, money};  
+  return dispatch => {
+    dispatch({
+      type: 'SEND_HISTORY',
+      nameSend, nameReceive, money
+    });
+  };
+  // return { type: 'SEND_HISTORY' , nameSend, nameReceive, money};  
 }
 export function clearSendHistory(){
-  return {type: 'CLEAR_SEND_HISTORY'};
+  return dispatch => {
+    dispatch({
+      type: 'CLEAR_SEND_HISTORY'
+    });
+  };
+  // return {type: 'CLEAR_SEND_HISTORY'};
 }
 export function addReceiveHistory(nameSend, nameReceive, money) {
-  return { type: 'RECEIVE_HISTORY' , nameSend, nameReceive, money};  
+  return dispatch => {
+    dispatch({
+      type: 'RECEIVE_HISTORY',
+      nameSend, nameReceive, money
+    });
+  };
+  // return { type: 'RECEIVE_HISTORY' , nameSend, nameReceive, money};  
 }
 export function clearReceiveHistory(){
-  return {type: 'CLEAR_RECEIVE_HISTORY'};
+  return dispatch => {
+    dispatch({
+      type: 'CLEAR_RECEIVE_HISTORY'
+    });
+  };
+  // return {type: 'CLEAR_RECEIVE_HISTORY'};
 }
